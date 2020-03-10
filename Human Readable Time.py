966b23e32372ef110dbf17c489edfb4c4335f4bb
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-03-10 00:46:01 +0000

    Human Readable Time

--- Human Readable Time.py
+++ Human Readable Time.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python3
+import math
+
+
 def make_readable(seconds):
-    # Do something
+    hh = int(math.modf(seconds / 3600)[1])
+    mm = int(math.modf((seconds - hh * 3600) / 60)[1])
+    ss = int(str((seconds - hh * 3600) - (mm * 60)))
+    return "{:02}:{:02}:{:02}".format(hh, mm, ss)
